663f428a86ec83b337dc7767b9b551adaa8f1390
soval_000 <soval_000@Dunkan>
2015-12-30 20:06:48 +0500

    norm

--- FirstApplet/src/MyFirstApplet.java
+++ FirstApplet/src/MyFirstApplet.java
@@ -13,7 +13,5 @@ public class MyFirstApplet extends javax.swing.JApplet {
 	{
 		g.drawString("Applet Start: ", 20, 15);
 		g.drawString(calendar.getTime().toString(), 20, 35);
-		//υσι
-		// MotherFucker
 	}
 }
