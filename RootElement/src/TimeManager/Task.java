839c39e5007b3c19dfc837cfccc9a438bc8e0465
soval_000 <soval_000@Dunkan>
2016-01-02 13:52:10 +0500

    эксперементирую

---
+++ RootElement/src/TimeManager/Task.java
@@ -0,0 +1,38 @@
+package TimeManager;
+
+public class Task {
+
+	/**
+	 * 
+	 */
+	public String date;
+	/**
+	 * 
+	 */
+	public String text;
+	/**
+	 * Getter of date
+	 */
+	public String getDate() {
+	 	 return date; 
+	}
+	/**
+	 * Setter of date
+	 */
+	public void setDate(String date) { 
+		 this.date = date; 
+	}
+	/**
+	 * Getter of text
+	 */
+	public String getText() {
+	 	 return text; 
+	}
+	/**
+	 * Setter of text
+	 */
+	public void setText(String text) { 
+		 this.text = text; 
+	} 
+
+}
